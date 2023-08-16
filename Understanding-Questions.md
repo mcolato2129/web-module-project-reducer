b6319e7b87c5329d60fe63b5dc3501a2b12d9efc
Marlon Colato <mcolato2104@gmail.com>
2023-08-16 15:20:07 -0400

    Answered quest. in quest. folder added Aply#

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,9 @@
 # Understanding Questions:
 1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
 * The user presses the 1 button.
-* 
+* The onClick method is called to whatever it is pointing to
+* The onClick function uses the 'dispatch' with the use reducer method to find our created action thet we imported
+* Once it is found it goes to the reducer function and looks for the 'case' that we created and runs the logic written inside of it
 ...
 
 * TotalDisplay shows the total plus 1.
--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 import reducer, {initialState} from '../reducers'; /* In order to have access to the reducer and state in the reducers file we needed to import Both Of them from the reducers file.*/ 
-import { ADD_ONE } from '../actions'; /* need acces to my created actions from the actions folder so they need to get imported into this file from there folder to be able to be used here in this code.  */
+import { ADD_ONE, APPLY_NUMBER } from '../actions'; /* need acces to my created actions from the actions folder so they need to get imported into this file from there folder to be able to be used here in this code.  */
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState) /* useReducer has to be imported along with react. useReducer always gets [state, dislay]. the reducer function and state is made in the 'reducers' file. */
